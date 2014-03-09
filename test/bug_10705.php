833b44a24c8f1b44ad956d719e80fd5709b1996f
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-03-09 20:14:34 +1030

    Add a basic test for Bug #10705

---
+++ test/bug_10705.php
@@ -0,0 +1,26 @@
+<?php
+require 'MP3/Id.php';
+
+$mp3 = new MP3_Id();
+
+$data = $mp3->read(dirname(__FILE__) . '/test.mp3');
+
+$expected = array(
+  "TAG" => "TAG", 
+  "NAME" => "Test track", 
+  "ARTISTS" => "Test",
+  "ALBUM" => "The Tests Unplugged",
+  "YEAR" => "2955",
+  "COMMENT" => "Test comment",
+  "TRACK" => 1,
+  "GENRENO" => 74,
+  "GENRE" => "Acid Jazz"
+);
+
+foreach ($expected as $key => $value) {
+  assert($value === $data[$key], "Expected $value but got $data[$key]");
+  if ($value !==  $data[$key]) {
+    var_dump($value);
+    var_dump($data[$key]);
+  }
+}
